4c9999b991d14f97018ebcf8ccdd56e6b5fee6d8
RLisandro <rezendecazuza@gmail.com>
2025-04-06 14:12:51 -0300

    Acrescentação de idade e telefone

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-async function criarEPopularTabelaUsuarios(nome, sobrenome, idade) {
+async function criarEPopularTabelaUsuarios(nome, sobrenome, idade, telefone) {
   const db = await open({
     filename: "./banco.db",
     driver: sqlite3.Database
   });
   db.run(
-    `CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT, idade INTEGER)`
+    `CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT, idade TEXT, telefone TEXT)`
   );
-  db.run(`INSERT INTO usuarios (nome, sobrenome, idade) VALUES (?,?,?)`, [
-    nome,
-    sobrenome,
-    idade
-  ]);
+  db.run(
+    `INSERT INTO usuarios (nome, sobrenome, idade, telefone) VALUES (?,?,?,?)`,
+    [nome, sobrenome, idade, telefone]
+  );
 }
-criarEPopularTabelaUsuarios("Daniel", "Porto", 25)
+criarEPopularTabelaUsuarios("Daniel", "Porto", "25");
+criarEPopularTabelaUsuarios("Lucas", "Porto", "25", "99999999999")
   .then(() => console.log("Tabela criada e populada com sucesso!"))
   .catch((err) => console.error("Erro ao criar ou popular a tabela:", err));
